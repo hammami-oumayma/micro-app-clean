a00479544c6ffc14043aef701bdbda573d01b4ba
your_username <your_email>
2026-04-01 15:33:07 +0100

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();  // ← charge le .env
+import mongoose from "mongoose";
+
+import { app } from "./app";
+
+const start = async () => {
+  console.log("starting......");
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined!");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined!");
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDb!");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(4000, () => {
+    console.log("Listening on port 4000!!!");
+  });
+};
+
+start();
---
+++ orders/src/index.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { rabbitWrapper } from "./rabbit-wrapper";
+import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
+import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
+import { ExpirationCompleteListener } from "./events/listeners/expiration-complete-listener";
+import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
+import dotenv from "dotenv";
+dotenv.config();  // ← charge le .env
+async function start() {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+  if (!process.env.RABBITMQ_URL) {
+    throw new Error("RABBITMQ client must be defined");
+  }
+
+  try {
+    await rabbitWrapper.connect(process.env.RABBITMQ_URL);
+
+    rabbitWrapper.client.on("close", () => {
+      console.log("RABBITMQ connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => rabbitWrapper.client.close());
+    process.on("SIGTERM", () => rabbitWrapper.client.close());
+
+    new TicketCreatedListener(rabbitWrapper.client).consumeMessage();
+    new TicketUpdatedListener(rabbitWrapper.client).consumeMessage();
+    new ExpirationCompleteListener(rabbitWrapper.client).consumeMessage();
+    new PaymentCreatedListener(rabbitWrapper.client).consumeMessage();
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3005, () => {
+    console.log("Listening on port 3005!!!");
+  });
+}
+
+start();
